d2085c793d84881931ef307729dd9bb06374ee5e
Manuel Lazo <gardo984@gmail.com>
2019-05-24 03:52:13 -0500

    Interfaces part 2

--- app/src/main/java/com/example/utp_android1/Activity_CreateUser.java
+++ app/src/main/java/com/example/utp_android1/Activity_CreateUser.java
@@ -1,7 +1,9 @@
 package com.example.utp_android1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class Activity_CreateUser extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class Activity_CreateUser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__create_user);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        Intent intent = new Intent(getApplicationContext(),Activity_Welcome.class) ;
+        startActivityForResult(intent,0);
+        return true ;
     }
 }
--- app/src/main/java/com/example/utp_android1/Activity_Login.java
+++ app/src/main/java/com/example/utp_android1/Activity_Login.java
@@ -1,7 +1,9 @@
 package com.example.utp_android1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class Activity_Login extends AppCompatActivity {
 
@@ -9,6 +11,13 @@ public class Activity_Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__login);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        Intent intent = new Intent(getApplicationContext(),Activity_Welcome.class) ;
+        startActivityForResult(intent,0);
+        return true ;
     }
 
 
